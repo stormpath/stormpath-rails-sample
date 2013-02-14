232740423208cfd7d76c62023fedf6582fe684d4
Denis Grankin <liquidautumn@gmail.com>
2013-02-14 16:59:53 +0200

    updated devise-stormpath version, a bit of cleanup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ActiveRecord::Base
-  include Stormpath::Rails::Account
-
   devise :stormpath_authenticatable, :stormpath_recoverable, :stormpath_confirmable, :registerable, :rememberable
 
 end
